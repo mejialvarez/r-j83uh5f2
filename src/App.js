1dc63a9ff1a7ae0595b1736dfd5895c4f878874c
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2017-03-11 15:36:21 -0500

    Adds updateCounter method

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0
+    }
+  }
+
+  updateCounter(event) {
+    this.setState({
+      counter: event.target.value.length
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.updateCounter.bind(this)}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
